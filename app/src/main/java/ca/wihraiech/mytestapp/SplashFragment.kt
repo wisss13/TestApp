e072ae7b522b38986da328399d67fb4c25e6b375
wissem.hraiech <wissem.hraiech@quebecormedia.com>
2020-12-02 17:57:07 -0500

    add splash screen

--- app/src/main/java/ca/wihraiech/mytestapp/SplashFragment.kt
+++ app/src/main/java/ca/wihraiech/mytestapp/SplashFragment.kt
@@ -7,7 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import ca.wihraiech.mytestapp.fragments.SplashFragmentDirections
 
 class SplashFragment : Fragment() {
 
--- app/src/main/java/ca/wihraiech/mytestapp/joke/JokeDetailsFragment.kt
+++ app/src/main/java/ca/wihraiech/mytestapp/joke/JokeDetailsFragment.kt
@@ -7,7 +7,6 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import ca.wihraiech.mytestapp.R
 import ca.wihraiech.mytestapp.common.loadUrl
-import ca.wihraiech.mytestapp.common.loadUrl2
 import kotlinx.android.synthetic.main.fragment_joke_details.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
